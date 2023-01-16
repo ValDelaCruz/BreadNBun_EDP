333398b92437e53fc50c83913f3d70923461d8be
vldeez <97102984+vldeez@users.noreply.github.com>
2023-01-16 15:53:11 +0700

    Add project files.

---
+++ BreadNBun/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace BreadNBun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dATAENTRYToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageProductToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tRANSACTIONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pURCHASEORDERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.mAINTENANCEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lOGOUTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(47)))), ((int)(((byte)(38)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1233, 82);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(495, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(738, 82);
+            this.panel2.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dATAENTRYToolStripMenuItem,
+            this.tRANSACTIONToolStripMenuItem,
+            this.pURCHASEORDERToolStripMenuItem,
+            this.mAINTENANCEToolStripMenuItem,
+            this.lOGOUTToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(738, 82);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dATAENTRYToolStripMenuItem
+            // 
+            this.dATAENTRYToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageProductToolStripMenuItem});
+            this.dATAENTRYToolStripMenuItem.Name = "dATAENTRYToolStripMenuItem";
+            this.dATAENTRYToolStripMenuItem.Size = new System.Drawing.Size(108, 78);
+            this.dATAENTRYToolStripMenuItem.Text = "DATA ENTRY";
+            // 
+            // manageProductToolStripMenuItem
+            // 
+            this.manageProductToolStripMenuItem.Name = "manageProductToolStripMenuItem";
+            this.manageProductToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.manageProductToolStripMenuItem.Text = "Manage Product";
+            // 
+            // tRANSACTIONToolStripMenuItem
+            // 
+            this.tRANSACTIONToolStripMenuItem.Name = "tRANSACTIONToolStripMenuItem";
+            this.tRANSACTIONToolStripMenuItem.Size = new System.Drawing.Size(101, 78);
+            this.tRANSACTIONToolStripMenuItem.Text = "INVENTORY";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bread \'n Bun Inventory System";
+            // 
+            // pURCHASEORDERToolStripMenuItem
+            // 
+            this.pURCHASEORDERToolStripMenuItem.Name = "pURCHASEORDERToolStripMenuItem";
+            this.pURCHASEORDERToolStripMenuItem.Size = new System.Drawing.Size(148, 78);
+            this.pURCHASEORDERToolStripMenuItem.Text = "PURCHASE ORDER";
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 82);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1233, 551);
+            this.panel3.TabIndex = 1;
+            // 
+            // mAINTENANCEToolStripMenuItem
+            // 
+            this.mAINTENANCEToolStripMenuItem.Name = "mAINTENANCEToolStripMenuItem";
+            this.mAINTENANCEToolStripMenuItem.Size = new System.Drawing.Size(126, 78);
+            this.mAINTENANCEToolStripMenuItem.Text = "MAINTENANCE";
+            // 
+            // lOGOUTToolStripMenuItem
+            // 
+            this.lOGOUTToolStripMenuItem.Name = "lOGOUTToolStripMenuItem";
+            this.lOGOUTToolStripMenuItem.Size = new System.Drawing.Size(79, 78);
+            this.lOGOUTToolStripMenuItem.Text = "LOGOUT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 633);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dATAENTRYToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageProductToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tRANSACTIONToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pURCHASEORDERToolStripMenuItem;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ToolStripMenuItem mAINTENANCEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lOGOUTToolStripMenuItem;
+    }
+}
+
---
+++ BreadNBun/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BreadNBun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            menuStrip1.BackColor = Color.FromArgb(78, 47, 38);
+            menuStrip1.ForeColor = Color.White;
+            menuStrip1.Cursor = Cursors.Hand;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int width = Screen.PrimaryScreen.Bounds.Width;
+            int height = Screen.PrimaryScreen.Bounds.Height;
+            this.Width = width;
+            this.Height = height - 40;
+            this.Top = 0;
+            this.Left = 0;
+        }
+    }
+}
